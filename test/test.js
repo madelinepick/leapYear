40f7def7d3ba28b751382314550780edd33c57da
Madeline Pickering <madelinepickering@gmail.com>
2016-02-14 13:32:55 -0700

    short test version

--- test/test.js
+++ test/test.js
@@ -4,16 +4,10 @@ var expect = require('chai').expect
 describe('leapYear', function(){
   it('today', function(){
     expect(code.leapYear(2016)).to.equal(true)
-  })
-})
-
-describe('leapYear', function(){
+  });
   it('even 400', function(){
     expect(code.leapYear(2000)).to.equal(true)
-  })
-})
-
-describe('leapYear', function(){
+  });
   it('even not 400', function(){
     expect(code.leapYear(1700)).to.equal(false)
   })
